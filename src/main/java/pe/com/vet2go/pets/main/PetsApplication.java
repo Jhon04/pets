f039878374c62ff2375f83497f18c247b8427903
ChugnasIvan <ivan@ad-comp.net>
2023-01-16 23:13:18 -0500

    cp main

--- src/main/java/pe/com/vet2go/pets/main/PetsApplication.java
+++ src/main/java/pe/com/vet2go/pets/main/PetsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetsApplication {
-
+	//CAMBIO
 	public static void main(String[] args) {
 		SpringApplication.run(PetsApplication.class, args);
 	}
