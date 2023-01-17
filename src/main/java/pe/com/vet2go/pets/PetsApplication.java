7ff8ea35886e41569085d51620b11c1f6ce8aad7
ChugnasIvan <ivan@ad-comp.net>
2023-01-17 00:52:12 -0500

    configuracion - down at java 1.8 - spring boot 2.7.7

--- src/main/java/pe/com/vet2go/pets/PetsApplication.java
+++ src/main/java/pe/com/vet2go/pets/PetsApplication.java
@@ -1,9 +1,10 @@
-package pe.com.vet2go.pets.main;
+package pe.com.vet2go.pets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class PetsApplication {
 	//CAMBIO
 	public static void main(String[] args) {
