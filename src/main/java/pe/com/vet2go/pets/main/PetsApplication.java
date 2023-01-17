cc5bb3c2fced6fec0094c1b89a37b67a62ec2dd0
ChugnasIvan <ivan@ad-comp.net>
2023-01-16 22:47:56 -0500

    Initial commit

---
+++ src/main/java/pe/com/vet2go/pets/main/PetsApplication.java
@@ -0,0 +1,13 @@
+package pe.com.vet2go.pets.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsApplication.class, args);
+	}
+
+}
